dbb1b47558e4db65d0287636d9d076899292f1dc
fluffur <flurffur@gmail.com>
2026-01-07 16:03:39 +0500

    fix avatars base url & change context key type

--- web/src/config.ts
+++ web/src/config.ts
@@ -3,7 +3,7 @@ const host = window.location.host;
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || `${protocol}//${host.replace(/:\d+$/, ':8080')}`;
 export const API_UPLOADS_BASE_URL = `${API_BASE_URL}/uploads`;
-export const API_AVATARS_BASE_URL = `${API_UPLOADS_BASE_URL}/avatars`;
+export const API_AVATARS_BASE_URL = `${API_UPLOADS_BASE_URL}/avatars/`;
 
 const wsProtocol = protocol === "https:" ? "wss:" : "ws:";
 export const WS_BASE_URL = import.meta.env.VITE_WS_BASE_URL || `${wsProtocol}//${host.replace(/:\d+$/, ':8080')}`;
